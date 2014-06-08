5bff2a8cad84972cfba2e679c70b2f4917389494
Alex S <bobylito@gmail.com>
2014-06-08 21:44:01 +0200

    Watch me maybe

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,19 +2,26 @@ module.exports = function (grunt) {
   grunt.initConfig({
     browserify: {
       options: {
-        transform: [ require('grunt-react').browserify ]
+        transform: [ require('grunt-react').browserify ],
+        bundleOptions:{
+          debug: true
+        }
       },
       app: {
         src : 'src/app.js',
         dest: 'js/app.js'
       }
-    }
+    },
+    watch: {
+      files: ['./src/*'],
+      tasks: ['build']
+    },
   })
 
   grunt.loadNpmTasks('grunt-browserify');
-  //grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  //grunt.registerTask('default', ['build', 'connect:server', 'watch']);
+  grunt.registerTask('default', ['build', 'watch']);
   grunt.registerTask('build', ['browserify:app']);
   //grunt.registerTask('serve', ['connect:server']);
 };
